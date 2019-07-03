cb1fe810d859ea4f67a1809382607a6f02dc585f
Antonio Capizzi <antoniocapizzi95@hotmail.it>
2019-07-03 19:47:31 +0200

    added dalay in test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -13,6 +13,8 @@ set -x
 # npm install --save-dev cross-env
 set +x
 
+sleep 5
+
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
