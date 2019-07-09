e06477bbd7c05ba5fbf385f8e688e6fa7a856d45
Antonio Capizzi <antoniocapizzi95@hotmail.it>
2019-07-09 15:13:44 +0200

    added home

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar'
 import TypoGraphy from '@material-ui/core/Typography';
-import NavBar from './components/navbar.js';
 import Apod from './components/apod/apod.js'
+import Home from './components/home.js'
 import {
   Route,
   NavLink,
@@ -18,43 +18,45 @@ class App extends Component {
   render() {
     return (
         <div>
-          <AppBar color="primary" position="static">
-            <Toolbar>
-              <TypoGraphy variant="title"
-                          color="inherit"
-              >
-                Space Viewer
-              </TypoGraphy>
-              <HashRouter>
-                <List component="nav">
-                  <ListItem component="div">
+          <HashRouter>
+            <AppBar color="primary" position="static">
+              <Toolbar>
+                <TypoGraphy variant="title"
+                            color="inherit"
+                >
+                  Space Viewer
+                </TypoGraphy>
 
-                    <ListItemText inset>
-                      <TypoGraphy color="inherit" variant="title">
-                        Home
-                      </TypoGraphy>
-                    </ListItemText>
+                  <List component="nav">
+                    <ListItem component="div">
+
+                      <ListItemText inset>
+                        <TypoGraphy color="inherit" variant="title">
+                          <NavLink to="/"> Home </NavLink>
+                        </TypoGraphy>
+                      </ListItemText>
 
 
-                    <ListItemText inset>
-                      <TypoGraphy color="inherit" variant="title">
-                        <NavLink to="/apod"> APOD </NavLink>
-                      </TypoGraphy>
-                    </ListItemText>
+                      <ListItemText inset>
+                        <TypoGraphy color="inherit" variant="title">
+                          <NavLink to="/apod"> APOD </NavLink>
+                        </TypoGraphy>
+                      </ListItemText>
 
-                    <ListItemText inset>
-                      <TypoGraphy color="inherit" variant="title">
-                        Asteroids
-                      </TypoGraphy>
-                    </ListItemText>
-                  </ListItem>
-                </List>
-                <div className="content">
-                  <Route path="/apod" component={Apod}/>
-                </div>
-              </HashRouter>
-            </Toolbar>
-          </AppBar>
+                      <ListItemText inset>
+                        <TypoGraphy color="inherit" variant="title">
+                          Asteroids
+                        </TypoGraphy>
+                      </ListItemText>
+                    </ListItem>
+                  </List>
+              </Toolbar>
+            </AppBar>
+            <div className="content">
+              <Route path="/" component={Home}/>
+              <Route path="/apod" component={Apod}/>
+            </div>
+          </HashRouter>
         </div>
     );
   }
---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>SpaceViewer Home</h1>
+                <div>
+                    Welcome to SpaceViewer
+                </div>
+            </div>
+        );
+    }
+}
+export default Home;
