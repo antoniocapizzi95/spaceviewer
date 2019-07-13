a9079a1238ef1d31bc0fac85f65577233074220d
Antonio Capizzi <antoniocapizzi95@hotmail.it>
2019-07-13 17:15:45 +0200

    try to add asteroids data

---
+++ src/components/asteroids/ShowData.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ShowData = props => (
+    <div>
+
+    </div>
+);
+
+export default ShowData;
--- src/components/asteroids/asteroids.js
+++ src/components/asteroids/asteroids.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import DateInput from "./DateInput.js";
+import ShowData from "./ShowData.js"
 
 class Asteroids extends Component {
     state = {
@@ -25,9 +26,10 @@ class Asteroids extends Component {
     }
     getAsteroids = date => {
 
-        fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date[0]}&end_date=${date[1]}8&api_key=${this.state.key} `)
+        fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date[0]}&end_date=${date[1]}&api_key=${this.state.key} `)
             .then(response => response.json())
-            .then(photoData => this.setState({ asteroids: photoData }));
+            .then(astData => this.setState({ asteroids: astData }))
+            .then(ast => console.log(this.state.asteroids));
     };
     render() {
         return (
@@ -35,7 +37,11 @@ class Asteroids extends Component {
                 <h1>Asteroids</h1>
                 <div>
                     <DateInput changeDate={this.changeDate}></DateInput>
-                    <p></p>
+
+                    {this.state.asteroids.near_earth_objects.map((value, index) => {
+                        return <li key={index}>{value}</li>
+                    })}
+                    <ShowData asteroids={this.state.asteroids.near_earth_objects}></ShowData>
                 </div>
 
             </div>
