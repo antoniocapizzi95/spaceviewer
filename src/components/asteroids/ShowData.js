2276e52fd9fa97fb6006416def55bb192a2011eb
Antonio Capizzi <antoniocapizzi95@hotmail.it>
2019-07-14 19:24:02 +0200

    added first asteroids view

--- src/components/asteroids/ShowData.js
+++ src/components/asteroids/ShowData.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ShowData = props => (
     <div>
-
+        {props.asteroids.id} - {props.asteroids.name} - {props.asteroids.close_approach_data[0].close_approach_date} - <a href={props.asteroids.nasa_jpl_url}>Link</a>
     </div>
 );
 
--- src/components/asteroids/asteroids.js
+++ src/components/asteroids/asteroids.js
@@ -14,9 +14,12 @@ class Asteroids extends Component {
         console.log(e.target);
         let dateFromInputInit = e.target[0].value;
         let dateFromInputEnd = e.target[1].value;
-        this.setState({ dateInit: dateFromInputInit });
-        this.setState({ dateEnd: dateFromInputEnd });
-        this.getAsteroids([this.state.dateInit, this.state.dateEnd]);
+        this.state.dateInit = dateFromInputInit
+        this.state.dateEnd = dateFromInputEnd
+        //this.setState({ dateInit: dateFromInputInit, dateEnd: dateFromInputEnd });
+        //this.setState({ dateEnd: dateFromInputEnd });
+        this.getAsteroids(this.state.dateInit, this.state.dateEnd);
+        //this.getAsteroids("2015-09-07", "2015-09-08");
     };
     componentDidMount() {
         fetch(`http://devopsresearch.tk:5001`)
@@ -24,24 +27,32 @@ class Asteroids extends Component {
             .then(key => this.setState({ key: key.key }))
             //.then(first => this.getFirstApod());
     }
-    getAsteroids = date => {
-
-        fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${date[0]}&end_date=${date[1]}&api_key=${this.state.key} `)
+    getAsteroids (dateInit, dateEnd) {
+        //console.log(dateInit)
+        console.log(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateInit}&end_date=${dateEnd}&api_key=${this.state.key}`)
+        fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateInit}&end_date=${dateEnd}&api_key=${this.state.key}`)
             .then(response => response.json())
-            .then(astData => this.setState({ asteroids: astData }))
+            .then(astData => this.setState({ asteroids: astData.near_earth_objects }))
             .then(ast => console.log(this.state.asteroids));
     };
     render() {
+        var arr = [];
+        var json = this.state.asteroids;
+        Object.keys(json).forEach(function(key) {
+            arr.push(json[key]);
+        });
         return (
             <div>
                 <h1>Asteroids</h1>
                 <div>
                     <DateInput changeDate={this.changeDate}></DateInput>
 
-                    {this.state.asteroids.near_earth_objects.map((value, index) => {
-                        return <li key={index}>{value}</li>
+                    {arr.map((value, index) => {
+                        return value.map((v, i) => {
+                            return <ShowData asteroids = {v}></ShowData>
+                        })
                     })}
-                    <ShowData asteroids={this.state.asteroids.near_earth_objects}></ShowData>
+
                 </div>
 
             </div>
