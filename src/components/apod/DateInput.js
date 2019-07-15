c18746561c01906cd14f655c0ac4a63effe1f5f4
Antonio Capizzi <antoniocapizzi95@hotmail.it>
2019-07-15 18:04:54 +0200

    added dateinput in material design

--- src/components/apod/DateInput.js
+++ src/components/apod/DateInput.js
@@ -1,10 +1,29 @@
 import React from "react";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const hei = {
+  height: '100%'
+};
+const marg = {
+    margin: '10px'
+};
 
 const DateInput = props => (
     <form onSubmit={props.changeDate}>
-        Enter a date (YYYY-MM-DD):
-        <input />
-        <input type="submit" />
+        <TextField
+            id="date"
+            label="Enter a date"
+            type="date"
+            className={hei}
+            defaultValue={new Date().toLocaleString()}
+            InputLabelProps={{
+                shrink: true,
+            }}
+        />
+        <Button variant="contained" type="submit" style={marg} className={hei}>
+            Ok
+        </Button>
     </form>
 );
 
--- src/components/asteroids/DateInput.js
+++ src/components/asteroids/DateInput.js
@@ -1,11 +1,43 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const hei = {
+    height: '100%'
+};
+const marg = {
+    margin: '10px'
+};
+
+const margright = {
+    marginRight: '10px'
+};
 
 const DateInput = props => (
     <form onSubmit={props.changeDate}>
-        Enter the start date and end date (YYYY-MM-DD):
-        <input />
-        <input />
-        <input type="submit" />
+        <TextField
+            id="dateInit"
+            label="Enter a start date"
+            type="date"
+            className={hei}
+            style={margright}
+            InputLabelProps={{
+                shrink: true,
+            }}
+        />
+        <TextField
+            id="date"
+            label="Enter a end date"
+            type="date"
+            className={hei}
+            style={margright}
+            InputLabelProps={{
+                shrink: true,
+            }}
+        />
+        <Button variant="contained" type="submit" style={marg} className={hei}>
+            Ok
+        </Button>
     </form>
 );
 
